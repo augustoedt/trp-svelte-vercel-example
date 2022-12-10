b4eaccb12f238d7bedfb0ffbfbbccd000a6e5e1d
Augusto Eduardo <augustoedt@gmail.com>
2022-12-10 08:59:29 -0300

    fix build script

--- src/lib/types/generated/user.ts
+++ src/lib/types/generated/user.ts
@@ -1,10 +1,10 @@
-import * as z from 'zod';
+import * as z from "zod"
 
 export const UserModel = z.object({
-	id: z.string(),
-	email: z.string().email({ message: 'Endereço de email inválido' }),
-	password: z.string().min(5, { message: 'Mínimo de 5 caractéres' }),
-	role: z.string(),
-	name: z.string().min(5, { message: 'Mínimo de 5 caractéres' }),
-	created: z.date()
-});
+  id: z.string(),
+  email: z.string().email({ message: "Endereço de email inválido" }),
+  password: z.string().min(5, { message: "Mínimo de 5 caractéres" }),
+  role: z.string(),
+  name: z.string().min(5, { message: "Mínimo de 5 caractéres" }),
+  created: z.date(),
+})
